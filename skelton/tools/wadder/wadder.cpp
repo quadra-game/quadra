5622324c5487bd77545bec04c8cbbc5806aac378
Pierre Phaneuf <pphaneuf@gmail.com>
2006-07-02 13:40:17 +0000

    Just use our own basename() function consistently, it's simpler.
    git-svn-id: http://quadra.googlecode.com/svn/trunk/quadra@270 8926ee93-8b47-0410-9975-e57e0fb0bd0c

--- skelton/tools/wadder/wadder.cpp
+++ skelton/tools/wadder/wadder.cpp
@@ -116,14 +116,12 @@ RCSID("$Id$")
 char *usage = "usage: wadder <working directory> <output res> <input text>\n";
 Resfile *wad;
 
-#ifndef HAVE_BASENAME
 const char *basename(const char* f) {
 	const char* p=(const char*)(f+strlen(f));
 	while(*p != '/' && *p != '\\' && p>=f)
 		p--;
 	return p+1;
 }
-#endif
 
 void addfile(const char* fname) {
 	Res_dos *res;
