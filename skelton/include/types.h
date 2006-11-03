dec80d6443da97dacacc6529211d2fb905b8a99d
Pierre Phaneuf <pphaneuf@gmail.com>
2006-11-03 12:02:40 +0000

    Updated the Win32 macro for strcasecmp and added the strncasecmp equivalent.
    git-svn-id: http://quadra.googlecode.com/svn/trunk/quadra@419 8926ee93-8b47-0410-9975-e57e0fb0bd0c

--- skelton/include/types.h
+++ skelton/include/types.h
@@ -48,7 +48,8 @@ typedef unsigned char Byte;
 #ifdef UGS_DIRECTX
 #define snprintf _snprintf
 #define vsnprintf _vsnprintf
-#define strcasecmp stricmp
+#define strcasecmp _stricmp
+#define strncasecmp _strnicmp
 typedef int socklen_t;
 #endif /* UGS_DIRECTX */
 
