5a51a8f5f6b72ebda9d0008eb4880d415d1dfd44
Pierre Phaneuf <pphaneuf@gmail.com>
2006-11-11 19:43:31 +0000

    Fixed a crash when accessing the players settings menu with some strange values in the configuration file.
    git-svn-id: http://quadra.googlecode.com/svn/trunk/quadra@440 8926ee93-8b47-0410-9975-e57e0fb0bd0c

--- skelton/common/inter.cpp
+++ skelton/common/inter.cpp
@@ -211,7 +211,8 @@ void Zone_state_text::draw() {
 	pan->draw();
 	video->vb->vline(x, y, h, 255);
 	video->vb->hline(y, x, w, 255);
-	fonts[last_val]->draw(state[last_val], pan->pan, CENTER, 0);
+  if (last_val < sizeof(fonts) / sizeof(*fonts))
+    fonts[last_val]->draw(state[last_val], pan->pan, CENTER, 0);
 }
 
 void Zone_state_text::leaved() {
