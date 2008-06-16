9ad77bf6bc41ad46aebfbb11e0000a60e267a834
Pierre Phaneuf <pphaneuf@gmail.com>
2008-06-16 04:15:43 +0000

    A feeble start at trying to cut Canvas down a bit.
    git-svn-id: http://quadra.googlecode.com/svn/branches/quadra-sdl@562 8926ee93-8b47-0410-9975-e57e0fb0bd0c

--- include/canvas.h
+++ include/canvas.h
@@ -35,12 +35,9 @@ class Packet_clientmoves;
 class Bloc;
 class Net_connection;
 class Zone;
-class Palette;
-class Bitmap;
 class Executor;
 class Overmind;
 class Inter;
-class Sprite;
 
 class Canvas: public CS, public Identifyable {
 public:
@@ -58,16 +55,59 @@ public:
 			nc = pnc;
 		}
 	};
-	Array<Watcher *> watchers;
+
+	void add_text_scroller(const char *st, int xoffset=4, int yoffset=0);
+	void calc_speed();
+	void set_next();
+	void set_message(const char *m1, const char *m2);
+	void change_level_single();
+	static void change_level(const int level, Palette *pal, Bitmap *bit);
+	void delete_bloc();
+	bool check_collide(Bloc *blo, Byte px, Byte py, Byte rot);
+	bool collide(Byte px, Byte py, Byte rot);
+	void clear_key_all();
+	void calc_shadow();
+	void reinit();
+	void restart();
+	void clear_tmp();
+	void give_line();
+	void blit_bloc(Bloc *blo);
+	void blit_level_up();
+	void blit_back();
+	void blit_flash();
+	void small_blit_bloc(Bloc *blo);
+	void small_blit_back();
+	void small_blit_flash();
+	Byte check_key(int i);
+	void clear_key(int i);
+	void unrelease_key(int i);
+	void dirt_rect(int x1, int y1, int w1, int h1);
+	void add_packet(Canvas *sender, Byte nb, Byte nc, Byte lx, Attack attack, Word hole_pos[]);
+	void start_moves();
+	void send_p_moves();
+	void start_byte();
+	void set_bit(int v);
+	void write_byte();
+	Canvas(int game_seed, Byte team, const char *nam, int ph_repeat, int pv_repeat, bool psmooth, bool pshadow, int phandicap, Net_connection *adr, int qplayer, bool wait_down);
+	Canvas(int qplayer, int game_seed, Palette *p);
+	virtual ~Canvas();
+	void set_canvas_pos(int px, int py, Bitmap *fo, Video_bitmap *s, Zone_next *z, Zone_next *z2, Zone_next *z3, Inter *in);
+	void hide();
+	bool islocal() const; 
 	void add_watcher(Watcher *w);
 	void remove_watcher(Net_connection *nc);
+
+	Array<Watcher *> watchers;
 	//Potato stuff (server only)
 	Byte potato_team_on_last_stamp;
 	Dword potato_lines;
-	Zone *z_lines, *z_potatolines, *z_linestot, *z_potatolinestot;
-	int team_potato_lines, team_potato_linestot;
+  Zone *z_lines;
+  Zone* z_potatolines;
+  Zone* z_linestot;
+  Zone* z_potatolinestot;
+  int team_potato_lines;
+  int team_potato_linestot;
 	bool should_remove_bonus; //Remove bonus when we have the chance
-
 	Dword gone_time;
   /* possible values of canvas->idle:
     0: busy, canvas not available (being recursed)
@@ -106,93 +146,77 @@ public:
   } bon[20];  // the waiting annoying lines
 	Byte last_x;  // position of the last dropped block (for line sending)
 	char snapshot[32*10*2+1]; // 32 lines * 10 columns * 2 chars per block + 1 string terminator
-	Word best_move; //MSB: depth, LSB: complexity
-	Word best_clean; //same here
-	Word best_recurse; //MSB: complexity, LSB: depth
-	Bloc* bloc, *next, *next2, *next3, *bloc_shadow;
+  Bloc* bloc;
+  Bloc* next;
+  Bloc* next2;
+  Bloc* next3;
+  Bloc* bloc_shadow;
 	Byte color_flash;
-	Zone_next* znext, *znext2, *znext3;
-	char msg1[64], msg2[64];
 	Dword frame_start;
-	Byte attacks[MAXPLAYERS], last_attacker;
+  Byte attacks[MAXPLAYERS];
+  Byte last_attacker;
 	// stuff for latest new handicap code (version 24)
 	enum { stamp_per_handicap = 3 };
 	Byte handicaps[MAXPLAYERS]; // stamp counter vis-a-vis other players
 	Byte handicap_crowd; // similar counter but for the crowdedness factor
+	// If you read this, get yourself a cookie, you deserve it!
 	Inter *inter;
 	Overmind *over;
 	Executor *myself;
 	int handicap;
-	int level, speed, player; // player: LOCAL player number (0 to 2)
+  int level;
+  int speed;
+  int player; // player: LOCAL player number (0 to 2)
 	int num_player; // num_player: net_list player number (0 to MAXPLAYERS)
-	int h_repeat, h_repeat_delay;
-	int v_repeat, v_repeat_delay;
+  int h_repeat;
+  int h_repeat_delay;
+  int v_repeat;
 	int continuous;
 	int level_up;
-	int side_speed, down_speed, depth, complexity, bonus;
+  int side_speed;
+  int down_speed;
+  int depth;
+  int complexity;
+  int bonus;
 	bool send_for_clean;
-	int x,y;
+  int x;
+  int y;
 	char name[40];
 	Byte player_hash[16];
 	char *long_name(bool handi=true, bool gone=true);
 	char team_name[40];
 	Byte team_hash[16];
-	Bitmap *fond;
-	Video_bitmap *screen;
-	SDL_Surface *sprlevel_up;
 	bool smooth;
 	bool shadow;
-	void add_text_scroller(const char *st, int xoffset=4, int yoffset=0);
-	void blind_all(Byte time);
-	void calc_speed();
-	void set_next();
-	void set_message(const char *m1, const char *m2);
-	void change_level_single();
-	static void change_level(const int level, Palette *pal, Bitmap *bit);
-	void delete_bloc();
-	void draw_block(int j, int i) const;
-	void init_block();
+
 	//After a call to collide (or check_collide) that returned
 	//  true, collide_side_only says if the collision
 	//  was solely with the canvas' side walls.
 	bool collide_side_only;
-	bool check_collide(Bloc *blo, Byte px, Byte py, Byte rot);
-	bool collide(Byte px, Byte py, Byte rot);
-	void clear_key_all();
-	void calc_shadow();
+	Buf da_moves;
+
+private:
+	void blind_all(Byte time);
+	void draw_block(int j, int i) const;
+	void init_block();
 	void init();
-	void reinit();
-	void restart();
-	void clear_tmp();
-	void give_line();
 	void step_bflash();
-	void blit_bloc(Bloc *blo);
-	void blit_level_up();
-	void blit_back();
-	void blit_flash();
-	void small_blit_bloc(Bloc *blo);
-	void small_blit_back();
 	void small_draw_block(int j, int i) const;
-	void small_blit_flash();
-	Byte check_key(int i);
-	void clear_key(int i);
-	void unrelease_key(int i);
-	void dirt_rect(int x1, int y1, int w1, int h1);
-	void add_packet(Canvas *sender, Byte nb, Byte nc, Byte lx, Attack attack, Word hole_pos[]);
+
+	Word best_move; //MSB: depth, LSB: complexity
+	Word best_clean; //same here
+	Word best_recurse; //MSB: complexity, LSB: depth
+  Zone_next* znext;
+  Zone_next* znext2;
+  Zone_next* znext3;
+  char msg1[64];
+  char msg2[64];
+  int v_repeat_delay;
+	Bitmap *fond;
+	Video_bitmap *screen;
+	SDL_Surface *sprlevel_up;
 	// multiplayer recording stuff
 	Packet_clientmoves *moves;
-	void start_moves();
-	void send_p_moves();
-	void start_byte();
-	void set_bit(int v);
-	void write_byte();
-	Buf da_moves;
-	Canvas(int game_seed, Byte team, const char *nam, int ph_repeat, int pv_repeat, bool psmooth, bool pshadow, int phandicap, Net_connection *adr, int qplayer, bool wait_down);
-	Canvas(int qplayer, int game_seed, Palette *p);
-	virtual ~Canvas();
-	void set_canvas_pos(int px, int py, Bitmap *fo, Video_bitmap *s, Zone_next *z, Zone_next *z2, Zone_next *z3, Inter *in);
-	void hide();
-	bool islocal() const; 
 };
 
 #endif
