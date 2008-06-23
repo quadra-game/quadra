fc93bd2429fc837f96036b6b43ddc9e01fb1986a
Pierre Phaneuf <pphaneuf@gmail.com>
2008-06-23 15:56:08 +0000

    Oops, forgot to increment the version number!
    git-svn-id: http://quadra.googlecode.com/svn/trunk/quadra@609 8926ee93-8b47-0410-9975-e57e0fb0bd0c

--- include/version.h
+++ include/version.h
@@ -2,6 +2,6 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 2
 #define VERSION_PATCHLEVEL 0
-#define VERSION_EXTRA "rc4"
-#define VERSION_STRING "1.2.0rc4\0"
+#define VERSION_EXTRA "rc5"
+#define VERSION_STRING "1.2.0rc5\0"
 
