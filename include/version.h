6afcf9560d6ea16470469c4620a11ed363256e2e
Pierre Phaneuf <pphaneuf@gmail.com>
2008-11-15 21:13:49 +0000

    Updated the version number to 1.2.0rc6.
    git-svn-id: http://quadra.googlecode.com/svn/trunk/quadra@647 8926ee93-8b47-0410-9975-e57e0fb0bd0c

--- include/version.h
+++ include/version.h
@@ -2,6 +2,6 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 2
 #define VERSION_PATCHLEVEL 0
-#define VERSION_EXTRA "rc5"
-#define VERSION_STRING "1.2.0rc5\0"
+#define VERSION_EXTRA "rc6"
+#define VERSION_STRING "1.2.0rc6\0"
 
